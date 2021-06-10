f1f521a041f47d658f0568e43bb696a5f0cdb177
Ibrahim.H <bitsnaps@yahoo.fr>
2021-06-10 20:16:13 +0100

    convert to groovy project

---
+++ src/ratpack/Ratpack.groovy
@@ -0,0 +1,28 @@
+// import ratpack.guice.Guice
+// import ratpack.server.RatpackServer
+import static ratpack.groovy.Groovy.ratpack
+import ratpack.thymeleaf.ThymeleafModule
+import static ratpack.thymeleaf.Template.thymeleafTemplate
+
+ratpack {
+  serverConfig {
+    findBaseDir('thymeleaf')
+  }
+
+  bindings {
+    module(ThymeleafModule)
+  }
+
+  handlers {
+
+    get {
+      render(thymeleafTemplate([
+              "title":"Hello, Ratpack!",
+              "header":"Hello, Ratpack!",
+              "text":"This template got rendered using Thymeleaf"
+              ], "home"))
+
+    } // get
+
+  } //handlers
+}
